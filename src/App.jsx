0d106da0f698da1d5b75573e9dd3b781192d9aef
roshan <roshan.koirala@srisripublications.com>
2024-05-27 16:11:15 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Happiness from './Components/Posters/Happiness.jsx'
 import Navbar from './Components/Posters/navbar.jsx'
-import ImageUpload from './Components/upload image/imageupload.jsx'
+
 
 
 
@@ -16,7 +16,7 @@ function App() {
       <div>
         <Navbar />
         <Happiness />
-        <ImageUpload />
+      
      
      
       </div>
